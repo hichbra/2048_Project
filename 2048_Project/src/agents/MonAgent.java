3a94cbf3e6d1bd5423c116cdcebda65df8fc37b8
Hichbra <hicham.brahimi@laposte.net>
2015-02-06 18:08:13 +0100

    Ajout de Jade

---
+++ 2048_Project/src/agents/MonAgent.java
@@ -0,0 +1,12 @@
+package agents;
+
+import jade.core.Agent;
+
+@SuppressWarnings("serial")
+public class MonAgent extends Agent
+{
+	protected void setup()
+	{
+		System.out.println("Hello !");
+	}
+}
