ddb0d8e9a399e7957d7b85e6107ca3e04552571a
Hichbra <hicham.brahimi@laposte.net>
2015-02-06 18:47:47 +0100

    plateau de jeu init

--- 2048_Project/src/metier/Plateau.java
+++ 2048_Project/src/metier/Plateau.java
@@ -1,10 +1,52 @@
 package metier;
 
+import java.util.ArrayList;
+
 public class Plateau
 {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	ArrayList<ArrayList<Integer>> plateau ;
+	
+	public Plateau()
+	{
+		this.plateau = new ArrayList<ArrayList<Integer>>();
+		
+		// Creation des lignes du plateau
+		for( int i = 0 ; i < 4 ; i++ )
+			this.plateau.add(new ArrayList<Integer>());
+		
+		initPlateau();
+	}
+	
+	private void initPlateau() 
+	{
+		for( ArrayList<Integer> ligne : plateau ) // pour toutes les lignes
+		{
+			// On les cases à 0
+			for( int i = 0 ; i < 4 ; i++ )
+				ligne.add(0);
+		}
+	}
 
+	public String toString()
+	{
+		String s = "" ;
+		
+		for( ArrayList<Integer> ligne : plateau ) // pour toutes les lignes
+		{
+			for ( int cellule : ligne ) // pour toutes les cellules de la ligne
+				s += cellule+" ";
+			
+			s += "\n" ;
+		}
+		
+		return s ;
+	}
+	
+	public static void main(String[] args)
+	{
+		Plateau p = new Plateau() ;
+		
+		System.out.println(p);
 	}
 
 }
